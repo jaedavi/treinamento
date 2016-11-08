71a340bb1050d769f54a18ceffb07d73113c8139
jaedavi <jaelsondavidson@hotmail.com>
2016-11-08 12:03:31 -0200

    State and city tables created

---
+++ database/migrations/2016_11_08_134926_create_locations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('state');
+            $table->char('abbr', 2)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('cities', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('city');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cities');
+        Schema::drop('states');
+    }
+}
