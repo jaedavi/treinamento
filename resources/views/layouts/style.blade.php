79dd4884d87fe584b2113bc1d8feed56ccee0624
Eduardo Redressa <edustrd@gmail.com>
2016-11-21 12:36:39 -0200

    Add members

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -38,7 +38,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">CLIENTE <span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="/members">CLIENTE <span class="sr-only">(current)</span></a></li>
         <li><a href="#">DADOS DO CLIENTE</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
